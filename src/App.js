c3cba60116898be41df0aaadad0e19b3c69a6e4f
ReshmaShrema <shremareshma@gmail.com>
2023-03-07 20:07:12 +0530

    add expenseItem component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./Components/ExpenseItem";
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+      <div className="App">
+        <ExpenseItem/>
+          <h2>Let's get started!</h2>
+      </div>
   );
 }
 
