880055066ef76e4fbf32bfc43d1e826232759f2e
ReshmaShrema <shremareshma@gmail.com>
2023-03-08 12:09:04 +0530

    adding event handler

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,20 @@
 import ExpenseDate from '../Date/ExpenseDate';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card/Card';
 import './ExpenseItem.css';
 const ExpenseItem=(props)=> {
+    
+    const clickHandler=()=>{
+        console.log('Clicked');
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
+                <button onClick={()=>{console.log('cliked!!!')}}>Change </button>
             </div>
         </Card>
     );
