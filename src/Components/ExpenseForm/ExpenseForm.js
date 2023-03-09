02ad8d962e280a12d54f87660b052af6ac91fe76
ReshmaShrema <shremareshma@gmail.com>
2023-03-09 12:56:58 +0530

    default browser behaviour setting,two way binding

--- src/Components/ExpenseForm/ExpenseForm.js
+++ src/Components/ExpenseForm/ExpenseForm.js
@@ -1,48 +1,60 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 const ExpenseForm = () => {
-    //      const[enteredTitle,setEnteredTitle]=useState('');
-    //  const[enteredDate,setEnteredDate]=useState('');
-    //      const [enteredAmount, setEnteredAmount] = useState('');
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: '',
-    });
+       const[enteredTitle,setEnteredTitle]=useState('');
+     const[enteredDate,setEnteredDate]=useState('');
+          const [enteredAmount, setEnteredAmount] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    //});
 
     const titleChangeHandler = (e) => {
-        // setEnteredTitle(e.target.value)
+         setEnteredTitle(e.target.value)
         //setUserInput({ ...userInput,enteredTitle: e.target.value});
-        setUserInput((prevState) => {
-            return { ...prevState, enteredTitle: e.target.value };
-        });
-        console.log(e.target.value);
+        //setUserInput((prevState) => {
+        //return { ...prevState, enteredTitle: e.target.value };
+        //});
+        //console.log(e.target.value);
     };
     const dateChangeHandler = (e) => {
-        // setEnteredDate(e.target.value)
+        setEnteredDate(e.target.value)
         //setUserInput({ ...userInput, enteredDate: e.target.value });
-         setUserInput((prevState) => {
-             return { ...prevState, enteredDate: e.target.value };
-         });
-        console.log(e.target.value);
+        //  setUserInput((prevState) => {
+        //      return { ...prevState, enteredDate: e.target.value };
+        //  });
+        //console.log(e.target.value);
     };
     const amountChangeHandler = (e) => {
-        // setEnteredAmount(e.target.value);
+        setEnteredAmount(e.target.value);
         //setUserInput({ ...userInput, enteredAmount: e.target.value });
-         setUserInput((prevState) => {
-             return { ...prevState, enteredAmount: e.target.value };
-         });
+        //  setUserInput((prevState) => {
+        //      return { ...prevState, enteredAmount: e.target.value };
+        //  });
         console.log(e.target.value);
     };
+    const submitHandler=(event)=>{
+        event.preventDefault();
+        const expenseDate={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate)
+        }
+        setEnteredAmount('');
+        setEnteredDate('')
+        setEnteredTitle('')
+        console.log(expenseDate)
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
                     <input
                         type="text"
                         onChange={titleChangeHandler}
-                        value={userInput.enteredTitle}
+                        value={enteredTitle}
                     />
                 </div>
                 <div className="new-expense__control">
@@ -52,7 +64,7 @@ const ExpenseForm = () => {
                         type="number"
                         min="0.01"
                         step="0.01"
-                        value={userInput.enteredAmount}
+                        value={enteredAmount}
                     />
                 </div>
                 <div className="new-expense__control">
@@ -61,7 +73,7 @@ const ExpenseForm = () => {
                         type="date"
                         min="2019-01-01"
                         max="20222-12-31"
-                        value={userInput.enteredDate}
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
